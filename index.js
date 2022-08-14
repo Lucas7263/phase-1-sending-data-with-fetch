5a6a91c5427c598a8d08a444c4480d9d97407f15
Lucas <lucaswoodyard@yahoo.com>
2022-08-13 20:25:36 -0500

    commit tree 93a04f8abaf4c331579c1f520c640050ff286385

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Add your code here
+function submitData(userName, userEmail) {
+    const userInfo = {
+        method: "POST",
+        
+        headers:  {
+            "Content-Type": "application/json",
+            "Accept": "application/json",
+        },
+        
+        body: JSON.stringify({
+            name: `${userName}`,
+            email: `${userEmail}`
+        })
+    };
+    return window.fetch('http://localhost:3000/users', userInfo)
+    .then(function (res) {
+        return res.json();
+    })
+    .then(function (data) {
+        return Object.keys
+       
+
+    })
+}
+console.log(submitData)
+submitData('Jake1', 'Jake1@yahoo.com');
